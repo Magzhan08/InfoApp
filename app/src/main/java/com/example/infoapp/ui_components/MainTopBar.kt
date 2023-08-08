1762c4b525544b330d8ebfd930980f1705008bcf
Magzhan Nurbapa <magzhan270819@gmail.com>
2023-08-08 16:45:35 +0600

    Adding Dagger hilt to the project

---
+++ app/src/main/java/com/example/infoapp/ui_components/MainTopBar.kt
@@ -0,0 +1,55 @@
+package com.example.infoapp.ui_components
+
+import androidx.compose.material.IconButton
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+@Composable
+fun MainTopBar(title: String, scaffoldState: ScaffoldState) {
+    val coroutineScope = rememberCoroutineScope()
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontSize = 20.sp
+            )
+        },
+        backgroundColor = Color.White,
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    coroutineScope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu"
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = {
+
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Menu"
+                )
+            }
+        }
+    )
+}
